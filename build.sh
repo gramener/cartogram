944ea3e30302e9982163e916363025a16a886198
S Anand <root.node@gmail.com>
2013-02-15 10:37:55 +0530

    Replaced metadata.json with the richer metadata.csv

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # Convert shape files into other formats
 
-SOURCE=/d/site/gramener.com/viz/maps/data/india-constituencies/
+SOURCE=/d/site/gramener.com/viz/maps/data/
 TOPOJSON=D:/node_modules/topojson/bin/topojson
 
 # The below paths are no longer used.
@@ -50,6 +50,27 @@ function topojson_ac {
     done
 }
 
+# Create topojson files for administrative boundaries under maps/
+function topojson_adm {
+    for shape in `find $SOURCE -name '*_PC.shp'`
+    do
+        SRC=`cygpath -aw $shape`
+        OUT="maps/`basename $shape`"
+        OUT="${OUT%.*}".json
+        node $TOPOJSON \
+            -p ST_CODE \
+            -p PC_NO \
+            -p ST_NAME \
+            -p PC_NAME \
+            -p PC_TYPE \
+            -p AREA \
+            --simplify-proportion 0.15 \
+            --quantization 2000 \
+            --out $OUT \
+            $SRC
+    done
+}
+
 # Create CSV metadata file for all topojson files under maps/
 function topojson_meta {
     python <<EOF
@@ -58,37 +79,33 @@ import csv
 import json
 import glob
 
-file_state = {}
 state_name = {}
 properties = []
 for filename in glob.glob('maps/*_PC.json') + glob.glob('maps/*_AC.json'):
     data = json.load(open(filename))
-    p = data['objects'].values()[0]['geometries'][0]['properties']
+
+    for g in data['objects'].values()[0]['geometries']:
+        properties.append(g['properties'])
+        properties[-1]['filename'] = os.path.split(filename)[-1]
+
+    # State names are not always present. Create a lookup table.
+    p = g['properties']
     if 'ST_NAME' in p:
         state_name[p['ST_CODE']] = p['ST_NAME']
-    file_state[os.path.split(filename)[-1]] = p['ST_CODE']
 
-    for p in data['objects'].values()[0]['geometries']:
-        properties.append(p['properties'])
-        properties[-1]['filename'] = filename
-
-for filename in file_state:
-    file_state[filename] = state_name[file_state[filename]]
-
-with open('maps/metadata.json', 'w') as fp:
-    fp.write(json.dumps(sorted(file_state.items())))
 
 fields = ['filename', 'ST_CODE', 'PC_NO', 'AC_NO', 'PC_TYPE', 'PC_NAME', 'AC_TYPE', 'AC_NAME', 'ST_NAME', 'AREA']
 with open('maps/metadata.csv', 'w') as fp:
     out = csv.DictWriter(fp, fields, lineterminator='\n')
     out.writerow({field:field for field in fields})
     for p in properties:
+        p['ST_NAME'] = state_name[p['ST_CODE']]
         out.writerow(p)
 
 EOF
 }
 
 
-# topojson_pc
-# topojson_ac
+topojson_pc
+topojson_ac
 topojson_meta
