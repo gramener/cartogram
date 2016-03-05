b5e3925753e44b8b9af153dbe5e11752d9f6f0ed
S Anand <root.node@gmail.com>
2016-03-05 16:24:58 +0530

    Increment shape names automatically to avoid conflict. Restructure file

--- shape.py
+++ shape.py
@@ -1,22 +1,18 @@
 """
 Create an Excel map from topojson files.
-
-Usage:
-
-    python shape.py file1.json file2.json ...
-
-TODO:
-- Account for shapes with holes (Outer Manipur in S14_PC.json)
 """
 from __future__ import print_function
 
+import io
 import os
 import sys
 import glob
 import math
 import json
 import win32com.client
+from collections import Counter
 
+# Define MS Office and Excel constants
 msoEditingAuto = 0x0
 msoSegmentLine = 0x0
 msoFalse = 0
@@ -26,6 +22,9 @@ msoThemeColorText1 = 13
 msoThemeColorBackground1 = 14
 msoThemeColorBackground2 = 16
 vbext_ct_StdModule = 1
+
+# Keep count of how many times each shape key has been used
+count = Counter()
 
 
 def projection(lon, lat):
@@ -44,6 +43,8 @@ def projection(lon, lat):
     rho0 = ((c - 2 * n * math.sin(phi0)) / n) ** .5
     x, y = rho * math.sin(theta), rho0 - rho * math.cos(theta)
     return x, -y
+
+
 
 
 def draw(base, topo, key):
@@ -85,10 +86,11 @@ def draw(base, topo, key):
             n_arcs = len(geom['arcs'])
             properties = geom.get('properties', {})
             name = key(properties)
+            names = []
 
             # Convert arcs of a geometry into array of points
             for i, arcgroup in enumerate(geom['arcs']):
-                # Consolidate shapes into a point list. Ignore holes
+                # Consolidate shapes into a point list. TODO: factor in holes
                 points = []
                 for arc in arcgroup:
                     # arc is an index into point coords. Positive values go
@@ -104,77 +106,90 @@ def draw(base, topo, key):
                 shape.Line.Weight = 0.25
                 shape.Line.ForeColor.ObjectThemeColor = msoThemeColorBackground1
 
-                shape.Name = name if n_arcs == 1 else name + str(i)
+                shapename = shape.Name = '{:s}_{:d}'.format(name, count[name])
+                names.append(shapename)
+                count[name] += 1
 
             # Group shapes if required
             if n_arcs > 1:
-                shape = base.Shapes.Range([name + str(i) for i in range(n_arcs)]).Group()
-                shape.Name = name
+                shape = base.Shapes.Range(names).Group()
+                shape.Name = name or 'NA'
 
             yield properties
 
 
-Application = win32com.client.Dispatch("Excel.Application")
-Workbook = Application.Workbooks.Add()
-# In Excel 2007 / 2010, Excel files have multiple sheets. Remove all but first
-for sheet in range(1, len(Workbook.Sheets)):
-    Workbook.Sheets[1].Delete()
+def main(args):
+    # Launch Excel
+    xl = win32com.client.Dispatch("Excel.Application")
+    workbook = xl.Workbooks.Add()
+    xl.Visible = msoTrue
 
-single_sheet = True
-keycols = ['ST_CODE', 'PC_NAME']
-propcol = {key: i for i, key in enumerate(keycols)}
-row = start_row = 4
+    # In Excel 2007 / 2010, Excel files have multiple sheets. Remove all but first
+    for sheet in range(1, len(workbook.Sheets)):
+        workbook.Sheets[1].Delete()
+
+    single_sheet = True
+    propcol = {key: i for i, key in enumerate(args.key)}
+    row = start_row = 4
+
+    def key(properties):
+        'Create a key by joining key columns from properties'
+        return ':'.join(properties.get(k, '') for k in args.key).title()
+
+    for pathspec in args.file:
+        for filename in glob.glob(pathspec):
+            print(filename)
+            with io.open(filename, encoding=args.encoding) as handle:
+                data = json.load(handle)
+            if single_sheet:
+                sheet = workbook.ActiveSheet
+            else:
+                sheet = workbook.Sheets.Add()
+                row = start_row
+            for prop in draw(sheet, data, key):
+                sheet.Cells(row, 1).Value = 0
+                for attr, val in prop.iteritems():
+                    if attr not in propcol:
+                        propcol[attr] = len(propcol)
+                    sheet.Cells(row, propcol[attr] + 2).Value = val
+                row += 1
+                sys.stdout.write('.')
+
+            sheet.Cells(start_row - 1, 1).Value = 'Value'
+            for attr, column in propcol.iteritems():
+                sheet.Cells(start_row - 1, column + 2).Value = attr
+
+            if not single_sheet:
+                sheet.Name = os.path.splitext(os.path.basename(filename))[0]
+
+    # Add visual basic code. http://www.cpearson.com/excel/vbe.aspx
+    # Requires Excel modification: http://support.microsoft.com/kb/282830
+    # to resolve error 'Programmatic Access to Visual Basic Project is not trusted'
+    vbproj = workbook.VBProject
+    for sheet in workbook.Worksheets:
+        codemod = vbproj.VBComponents(sheet.Name).CodeModule
+        for line, row in enumerate(open('shape.bas')):
+            codemod.InsertLines(line + 1, row)
+
+        # Set the gradient
+        sheet.Cells(1, 1).Value = 'Colors'
+        sheet.Cells(1, 2).Value = 0.0
+        sheet.Cells(1, 3).Value = 0.5
+        sheet.Cells(1, 4).Value = 1.0
+        sheet.Cells(1, 2).Interior.Color = 255      # Red
+        sheet.Cells(1, 3).Interior.Color = 65535    # Yellow
+        sheet.Cells(1, 4).Interior.Color = 5296274  # Green
 
 
-def key(properties):
-    'Create a key by joining key columns from properties'
-    return ':'.join(properties.get(k, '') for k in keycols).title()
+if __name__ == '__main__':
+    import argparse
 
+    parser = argparse.ArgumentParser(
+        description=__doc__.strip(),
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--key', nargs='*', default=[], help='Properties to be used as keys')
+    parser.add_argument('--encoding', help='Input topojson encoding', default='utf-8')
+    parser.add_argument('file', help='TopoJSON files', nargs='+')
+    args = parser.parse_args()
 
-for pathspec in sys.argv[1:]:
-    for filename in glob.glob(pathspec):
-        print(filename)
-        data = json.load(open(filename))
-        if single_sheet:
-            sheet = Workbook.ActiveSheet
-        else:
-            sheet = Workbook.Sheets.Add()
-            row = start_row
-
-        for prop in draw(sheet, data, key):
-            sheet.Cells(row, 1).Value = 0
-            for attr, val in prop.iteritems():
-                if attr not in propcol:
-                    propcol[attr] = len(propcol)
-                sheet.Cells(row, propcol[attr] + 2).Value = val
-            row += 1
-            sys.stdout.write('.')
-
-        sheet.Cells(start_row - 1, 1).Value = 'Value'
-        for attr, column in propcol.iteritems():
-            sheet.Cells(start_row - 1, column + 2).Value = attr
-
-        if not single_sheet:
-            sheet.Name = os.path.splitext(os.path.basename(filename))[0]
-
-    sys.stdout.write('\n')
-
-# Add visual basic code. http://www.cpearson.com/excel/vbe.aspx
-# Requires Excel modification: http://support.microsoft.com/kb/282830
-# to resolve error 'Programmatic Access to Visual Basic Project is not trusted'
-vbproj = Workbook.VBProject
-for sheet in Workbook.Worksheets:
-    codemod = vbproj.VBComponents(sheet.Name).CodeModule
-    for line, row in enumerate(open('shape.bas')):
-        codemod.InsertLines(line + 1, row)
-
-    # Set the gradient
-    sheet.Cells(1, 1).Value = 'Colors'
-    sheet.Cells(1, 2).Value = 0.0
-    sheet.Cells(1, 3).Value = 0.5
-    sheet.Cells(1, 4).Value = 1.0
-    sheet.Cells(1, 2).Interior.Color = 255      # Red
-    sheet.Cells(1, 3).Interior.Color = 65535    # Yellow
-    sheet.Cells(1, 4).Interior.Color = 5296274  # Green
-
-Application.Visible = msoTrue
+    main(args)
